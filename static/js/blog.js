08af3094f344a5979a695aec71cb8e67989054f6
Cosme Valera <cosmevalerareales@gmail.com>
2025-06-22 21:04:28 +0200

    Try reversed

--- static/js/blog.js
+++ static/js/blog.js
@@ -15,10 +15,10 @@ function attachHoverToPreviewInPhone() {
             const distanceFromBottom = docHeight - scrollBottom;
             
             // Anti-glitch logic: use different thresholds for entering vs exiting bottom state
-            if (!isAtBottom && distanceFromBottom < 40) {
+            if (!isAtBottom && distanceFromBottom < 60) {
                 // Entering bottom state
                 isAtBottom = true;
-            } else if (isAtBottom && distanceFromBottom > 60) {
+            } else if (isAtBottom && distanceFromBottom > 40) {
                 // Exiting bottom state (higher threshold to prevent bounce glitch)
                 isAtBottom = false;
             }
